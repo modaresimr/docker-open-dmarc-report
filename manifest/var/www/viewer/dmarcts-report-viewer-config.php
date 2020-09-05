9701840059681479d258e7658cce8c8ea4d06643
Flow86 <656249+Flow86@users.noreply.github.com>
2020-09-05 13:10:25 +0200

    update other config file too

--- manifest/var/www/viewer/dmarcts-report-viewer-config.php
+++ manifest/var/www/viewer/dmarcts-report-viewer-config.php
@@ -8,11 +8,11 @@ $dbhost=getenv('REPORT_DB_HOST');
 $dbname=getenv('REPORT_DB_NAME');
 $dbuser=getenv('REPORT_DB_USER');
 $dbpass=getenv('REPORT_DB_PASS');
-$dbport='3306';
+$dbport=getenv('REPORT_DB_PORT');
 
 $cssfile="default.css";
 
-$default_lookup = 1;  # 1=on 0=off (on is old behaviour )
+$default_hostlookup = 1;  # 1=on 0=off (on is old behaviour )
 $default_sort = 0;  # 1=ASCdening 0=DESCending (ASCending is default behaviour )
 
 ?>
